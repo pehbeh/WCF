1991832e0afd30f0f8cb3d086c354bacd63bb123
Cyperghost <olaf_schmitz_1@t-online.de>
2024-11-08 09:56:09 +0100

    Remove unused function `UserAvatarAction::validateUpload()` and `UserAvatarAction::upload()`

--- wcfsetup/install/files/lib/data/user/avatar/UserAvatarAction.class.php
+++ wcfsetup/install/files/lib/data/user/avatar/UserAvatarAction.class.php
@@ -3,14 +3,6 @@
 namespace wcf\data\user\avatar;
 
 use wcf\data\AbstractDatabaseObjectAction;
-use wcf\data\user\User;
-use wcf\system\exception\IllegalLinkException;
-use wcf\system\exception\PermissionDeniedException;
-use wcf\system\exception\UserInputException;
-use wcf\system\upload\AvatarUploadFileSaveStrategy;
-use wcf\system\upload\AvatarUploadFileValidationStrategy;
-use wcf\system\upload\UploadFile;
-use wcf\system\WCF;
 
 /**
  * Executes avatar-related actions.
@@ -27,66 +19,4 @@ use wcf\system\WCF;
  */
 class UserAvatarAction extends AbstractDatabaseObjectAction
 {
-    /**
-     * currently edited avatar
-     * @var UserAvatarEditor
-     */
-    public $avatar;
-
-    /**
-     * Validates the upload action.
-     */
-    public function validateUpload()
-    {
-        $this->readInteger('userID', true);
-
-        if ($this->parameters['userID']) {
-            if (!WCF::getSession()->getPermission('admin.user.canEditUser')) {
-                throw new PermissionDeniedException();
-            }
-
-            $user = new User($this->parameters['userID']);
-            if (!$user->userID) {
-                throw new IllegalLinkException();
-            }
-        }
-
-        // check upload permissions
-        if (!WCF::getSession()->getPermission('user.profile.avatar.canUploadAvatar') || WCF::getUser()->disableAvatar) {
-            throw new PermissionDeniedException();
-        }
-
-        /** @noinspection PhpUndefinedMethodInspection */
-        if (\count($this->parameters['__files']->getFiles()) != 1) {
-            throw new UserInputException('files');
-        }
-
-        // check max filesize, allowed file extensions etc.
-        /** @noinspection PhpUndefinedMethodInspection */
-        $this->parameters['__files']->validateFiles(new AvatarUploadFileValidationStrategy(
-            \PHP_INT_MAX,
-            \explode("\n", WCF::getSession()->getPermission('user.profile.avatar.allowedFileExtensions'))
-        ));
-    }
-
-    /**
-     * Handles uploaded attachments.
-     */
-    public function upload()
-    {
-        /** @var UploadFile $file */
-        $file = $this->parameters['__files']->getFiles()[0];
-        $saveStrategy = new AvatarUploadFileSaveStrategy((!empty($this->parameters['userID']) ? \intval($this->parameters['userID']) : WCF::getUser()->userID));
-        /** @noinspection PhpUndefinedMethodInspection */
-        $this->parameters['__files']->saveFiles($saveStrategy);
-
-        if ($file->getValidationErrorType()) {
-            return ['errorType' => $file->getValidationErrorType()];
-        } else {
-            return [
-                'avatarID' => $saveStrategy->getAvatar()->avatarID,
-                'url' => $saveStrategy->getAvatar()->getURL(96),
-            ];
-        }
-    }
 }
