1bc7e4789764921dd4025ac6c8543538bd95f2cf
Alexander Ebert <ebert@woltlab.com>
2024-11-14 10:42:15 +0100

    Add the typings for `<woltlab-core-notice>`
    Fixes #6067

--- ts/global.d.ts
+++ ts/global.d.ts
@@ -19,6 +19,8 @@ type IconMetadata = [Codepoint, HasRegularVariant];
 type IconSize = 16 | 24 | 32 | 48 | 64 | 96 | 128 | 144;
 type LoadingIndicatorIconSize = 24 | 48 | 96;
 
+type WoltlabCoreNoticeElementType = "error" | "info" | "success" | "warning";
+
 declare global {
   interface WoltLabTemplate {
     fetch(v: object): string;
@@ -107,6 +109,12 @@ declare global {
     set hideText(hideText: boolean);
   }
 
+  interface WoltlabCoreNoticeElement extends HTMLElement {
+    get type(): WoltlabCoreNoticeElementType;
+    set type(type: WoltlabCoreNoticeElementType);
+    get icon(): string;
+  }
+
   interface WoltlabCoreReactionSummaryElement extends HTMLElement {
     get objectId(): number;
     get objectType(): string;
@@ -133,6 +141,7 @@ declare global {
     "woltlab-core-file": WoltlabCoreFileElement;
     "woltlab-core-file-upload": WoltlabCoreFileUploadElement;
     "woltlab-core-loading-indicator": WoltlabCoreLoadingIndicatorElement;
+    "woltlab-core-notice": WoltlabCoreNoticeElement;
     "woltlab-core-pagination": WoltlabCorePaginationElement;
     "woltlab-core-google-maps": WoltlabCoreGoogleMapsElement;
     "woltlab-core-reaction-summary": WoltlabCoreReactionSummaryElement;
