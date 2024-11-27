402074219da625f25e442dd6bd4df00b552ed8f6
Olaf Braun <olaf_schmitz_1@t-online.de>
2024-11-27 08:35:02 +0100

    Update ts/WoltLabSuite/Core/Form/Builder/Field/Controller/BadgeColor.ts
    Co-authored-by: Alexander Ebert <ebert@woltlab.com>

--- ts/WoltLabSuite/Core/Form/Builder/Field/Controller/BadgeColor.ts
+++ ts/WoltLabSuite/Core/Form/Builder/Field/Controller/BadgeColor.ts
@@ -3,6 +3,7 @@
  * @copyright 2001-2024 WoltLab GmbH
  * @license   GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
  * @since     6.2
+ * @woltlabExcludeBundle all
  */
 
 export class BadgeColorPreview {
