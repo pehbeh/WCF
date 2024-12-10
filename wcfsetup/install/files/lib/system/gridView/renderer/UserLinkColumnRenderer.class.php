f06b7274d2540381440296bacd2ec6772ae64b72
Marcel Werk <burntime@woltlab.com>
2024-12-10 13:09:01 +0100

    Add renderer for object links

---
+++ wcfsetup/install/files/lib/system/gridView/renderer/UserLinkColumnRenderer.class.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace wcf\system\gridView\renderer;
+
+use wcf\system\cache\runtime\AbstractRuntimeCache;
+use wcf\system\cache\runtime\UserRuntimeCache;
+
+/**
+ * Formats the content of a column as a user link.
+ *
+ * @author      Marcel Werk
+ * @copyright   2001-2024 WoltLab GmbH
+ * @license     GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ * @since       6.2
+ */
+class UserLinkColumnRenderer extends ObjectLinkColumnRenderer
+{
+    #[\Override]
+    protected function getRuntimeCache(): AbstractRuntimeCache
+    {
+        return UserRuntimeCache::getInstance();
+    }
+}
