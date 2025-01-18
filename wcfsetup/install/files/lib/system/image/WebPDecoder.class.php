6f26278fed28a440aaeb7b9f775b3bc0b393364e
Alexander Ebert <ebert@woltlab.com>
2025-01-18 17:55:37 +0100

    Simplify the logic to construct the JPEG

--- wcfsetup/install/files/lib/system/image/WebPDecoder.class.php
+++ wcfsetup/install/files/lib/system/image/WebPDecoder.class.php
@@ -75,26 +75,21 @@ final class WebPDecoder
 
         // A tiny JPEG used as the host for the EXIF data.
         // See https://github.com/mathiasbynens/small/blob/267b39f682598eebb0dafe7590b1504be79b5cad/jpeg.jpg
-        $jpg1x1px = \hex2bin("ffd8ffdb004300030202020202030202020303030304060404040404080606050609080a0a090809090a0c0f0c0a0b0e0b09090d110d0e0f101011100a0c12131210130f101010ffc9000b080001000101011100ffcc000600101005ffda0008010100003f00d2cf20ffd9");
+        // This is a modified version without the leading 0xFF 0xD8 (SOI)!
+        $jpegBody = \hex2bin("ffdb004300030202020202030202020303030304060404040404080606050609080a0a090809090a0c0f0c0a0b0e0b09090d110d0e0f101011100a0c12131210130f101010ffc9000b080001000101011100ffcc000600101005ffda0008010100003f00d2cf20ffd9");
 
         // The image does not have a JFIF tag and instead directly starts with
-        // the quantization table (DQT, 0xFF xDB) after the start of image (SOI,
-        // 0xFF 0xD8).
-        //
-        // This means our offset is always 2. We could further optimize this by
-        // ommitting the SOI from the hex string but the \substr() is already
-        // quite cheap that it doesn't make sense to obscure the image further.
-        \assert($jpg1x1px[0] === "\xFF" && $jpg1x1px[1] === "\xD8");
-        \assert($jpg1x1px[2] === "\xFF" && $jpg1x1px[3] === "\xDB");
-        $offset = 2;
+        // the quantization table (DQT, 0xFF xDB). The SOI (start of image, 0xFF
+        // 0xD8) is prepended below to simpify the construction of the image.
+        \assert($jpegBody[0] === "\xFF" && $jpegBody[1] === "\xDB");
 
-        // Add the markers for the EXIF sequence in JPEGs.
-        $exifData = "\xFF\xE1\xC3\xEF\x45\x78\x69\x66\x00\x00" . $exifData;
+        $soiTag = "\xFF\xD8";
+        $exifTag = "\xFF\xE1\xC3\xEF\x45\x78\x69\x66\x00\x00";
 
         $exif = \exif_read_data(
             \sprintf(
                 "data://image/jpeg;base64,%s",
-                \base64_encode(\substr($jpg1x1px, 0, $offset) . $exifData . \substr($jpg1x1px, $offset)),
+                \base64_encode($soiTag . $exifTag . $exifData . $jpegBody),
             ),
         );
 
