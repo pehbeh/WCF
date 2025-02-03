5a7bfaf0da858db0ccba6e7973d7d87b180fe222
Cyperghost <olaf_schmitz_1@t-online.de>
2025-02-03 09:57:15 +0100

    Import interactions

--- wcfsetup/install/files/lib/system/gridView/admin/BBCodeMediaProviderGridView.class.php
+++ wcfsetup/install/files/lib/system/gridView/admin/BBCodeMediaProviderGridView.class.php
@@ -15,6 +15,8 @@ use wcf\system\gridView\GridViewRowLink;
 use wcf\system\gridView\renderer\DefaultColumnRenderer;
 use wcf\system\gridView\renderer\ObjectIdColumnRenderer;
 use wcf\system\interaction\admin\BBCodeMediaProviderInteractions;
+use wcf\system\interaction\Divider;
+use wcf\system\interaction\EditInteraction;
 use wcf\system\interaction\ToggleInteraction;
 use wcf\system\WCF;
 
