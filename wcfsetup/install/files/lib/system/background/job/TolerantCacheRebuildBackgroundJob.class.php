97f3691f21f1c0137a7dbc5a74a6751c1ba993b4
Cyperghost <olaf_schmitz_1@t-online.de>
2025-02-24 10:25:05 +0100

    Add `symfony/cache` composer package

---
+++ wcfsetup/install/files/lib/system/background/job/TolerantCacheRebuildBackgroundJob.class.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace wcf\system\background\job;
+
+/**
+ * @author Olaf Braun
+ * @copyright 2001-2025 WoltLab GmbH
+ * @license GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ * @since 6.2
+ */
+class TolerantCacheRebuildBackgroundJob
+{
+
+}
