b169b96838823e5c07b7f0692ed1c94076b83269
Cyperghost <olaf_schmitz_1@t-online.de>
2025-02-24 14:01:45 +0100

    Use the symfony cache contracts

--- wcfsetup/install/files/lib/system/background/job/TolerantCacheRebuildBackgroundJob.class.php
+++ wcfsetup/install/files/lib/system/background/job/TolerantCacheRebuildBackgroundJob.class.php
@@ -2,13 +2,59 @@
 
 namespace wcf\system\background\job;
 
+use wcf\system\cache\CacheHandler;
+use wcf\system\cache\tolerant\AbstractTolerantCache;
+
 /**
  * @author Olaf Braun
  * @copyright 2001-2025 WoltLab GmbH
  * @license GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
  * @since 6.2
  */
-class TolerantCacheRebuildBackgroundJob
+final class TolerantCacheRebuildBackgroundJob extends AbstractUniqueBackgroundJob
 {
+    public function __construct(
+        /** @var class-string<AbstractTolerantCache<array|object> */
+        public readonly string $cacheClass,
+        /** @var array<string, mixed> */
+        public readonly array $parameters = []
+    ) {
+    }
 
+    public function identifier(): string
+    {
+        $identifier = $this->cacheClass;
+        if (!empty($this->parameters)) {
+            $identifier .= '-' . CacheHandler::getInstance()->getCacheIndex($this->parameters);
+        }
+
+        return $identifier;
+    }
+
+    #[\Override]
+    public function newInstance(): static
+    {
+        return new TolerantCacheRebuildBackgroundJob($this->cacheClass, $this->parameters);
+    }
+
+    #[\Override]
+    public function queueAgain(): bool
+    {
+        return false;
+    }
+
+    #[\Override]
+    public function perform()
+    {
+        if (!\class_exists($this->cacheClass)) {
+            return;
+        }
+
+        $asyncCache = new $this->cacheClass(...$this->parameters);
+        if (!$asyncCache->needsRebuild()) {
+            return;
+        }
+
+        $asyncCache->rebuild();
+    }
 }
