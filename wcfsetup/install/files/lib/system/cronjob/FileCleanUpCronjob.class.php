7eb608c53f25b67f8e85237f3d7513207922e345
Marcel Werk <burntime@woltlab.com>
2025-04-25 16:58:32 +0200

    Add cleanup for old contact form attachments

--- wcfsetup/install/files/lib/system/cronjob/FileCleanUpCronjob.class.php
+++ wcfsetup/install/files/lib/system/cronjob/FileCleanUpCronjob.class.php
@@ -4,6 +4,7 @@ namespace wcf\system\cronjob;
 
 use wcf\data\cronjob\Cronjob;
 use wcf\data\file\FileEditor;
+use wcf\data\object\type\ObjectTypeCache;
 use wcf\system\WCF;
 
 /**
@@ -28,10 +29,34 @@ class FileCleanUpCronjob extends AbstractCronjob
         $statement->execute();
         $fileIDs = $statement->fetchAll(\PDO::FETCH_COLUMN);
 
+        if (\MODULE_CONTACT_FORM && \CONTACT_FORM_PRUNE_ATTACHMENTS > 0) {
+            $fileIDs = \array_merge($fileIDs, $this->getOldContactFileIDs());
+        }
+
         if ($fileIDs === []) {
             return;
         }
 
         FileEditor::deleteAll($fileIDs);
+    }
+
+    /**
+     * @return int[]
+     */
+    private function getOldContactFileIDs(): array
+    {
+        $sql = "SELECT  fileID
+                FROM    wcf1_file
+                WHERE   objectTypeID = ?
+                    AND uploadTime IS NOT NULL
+                    AND uploadTime < ?";
+        $statement = WCF::getDB()->prepare($sql);
+        $statement->execute([
+            ObjectTypeCache::getInstance()
+                ->getObjectTypeIDByName('com.woltlab.wcf.file', 'com.woltlab.wcf.contact.form'),
+            \TIME_NOW - (\CONTACT_FORM_PRUNE_ATTACHMENTS * 86_400),
+        ]);
+
+        return $statement->fetchAll(\PDO::FETCH_COLUMN);
     }
 }
